76b4fac3c99b1c71204fa779ddb88c4b29482a60
GabrielRhee11 <j.seok6313@gmailcom>
2020-05-22 20:10:53 +0900

    Update hsg1.py

--- HSG/hsg1.py
+++ HSG/hsg1.py
@@ -1,19 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-sigma_s = 1   ## volatility of domestic stocks
-lamda_s = 1  ## risk premium of domestic stocks (MPR)
-dz_s = 1 ## browninan motion of uncertainty of domestics stocks
-dt = 1 ## 만기/거래일
+sigma_S = 1   ## volatility of domestic stocks
+lamda_S = 1  ## risk premium of domestic stocks (MPR)
+dz_S = 1 ## browninan motion of uncertainty of domestics stocks
+T = 174
+D = 252
+dt = T/D ## 만기/거래일
 r = 0.0286 ## 단기이자율
 
-sigma_e = 1  ## volatility of domestic ESG stocks
-lamda_e = 1 ## risk premium of domestic ESG stocks
-dz_e = 1 ## browninan motion of uncertainty of domestics ESG stocks
+sigma_E = 1  ## volatility of domestic ESG stocks
+lamda_E = 1 ## risk premium of domestic ESG stocks
+dz_E = 1 ## browninan motion of uncertainty of domestics ESG stocks
 
 sigma_r = 1 ## 국내 명목 이자율에 대한 변동성
-k = 1 ## 국내 단기이자율의 평균으로의 회귀속도
-r_bar = 1 ## 국내 단기이자율의 장기평균
+kapa = 1 ## 국내 단기이자율의 평균으로의 회귀속도
+rbar = 1 ## 국내 단기이자율의 장기평균
 dz_r = 1 ## 국내 이자율에대한 불확실성을 나타내는 브라우니안 모션
 
 theta_s = 1 ## 국내주가수익률에 대한 시장의 위험가격 벡터
@@ -36,8 +38,8 @@ lamda_ef = 1 ## 해외 ESG주식에 대한 위험 프리미엄
 dz_ef = 1 ## 해외 ESG 주식에 대한 불확실성을 나타내는 브라우니안 모션
 
 sigma_rf = 1 ## 해외 명목 이자율에 대한 변동성
-kf = 1 ## 해외 단기이자율의 평균으로의 회귀속도
-rf_bar = 1 ## 해외 단기이자율의 장기평균
+kapaf = 1 ## 해외 단기이자율의 평균으로의 회귀속도
+rfbar = 1 ## 해외 단기이자율의 장기평균
 dz_rf = 1 ## 해외 이자율에대한 불확실성을 나타내는 브라우니안 모션
 
 theta_sf = 1 ## 해외주가수익률에 대한 시장의 위험가격 벡터
@@ -48,10 +50,13 @@ x_sf = 1 ## 해외주식의 자산배분
 x_ef = 1 ## 해외ESG주식의 자산배분
 x_rf = 1 ## 해외채권의 자산배분
 
-A_s = r+(sigma_s*lamda_s)
-B_s = sigma_s
-A_e = r+(sigma_e*lamda_e)
-B_e = sigma_e
 
-dS = (A_s*dt) + (B_s*dz_s)
-dE = (A_e*dt) + (B_e*dz_e)
+dS = (r+sigma_S*lamda_S)*dt + (sigma_S*dz_S) # 국내주식 GBM
+dE = (r+sigma_E*lamda_E)*dt + (sigma_E*dz_E) # 해외ESG  GBM
+dr = kapa*((rbar-r)*dt)+sigma_r*dz_r #이자율 vasicek 모형
+dM = -r*dt+pi
+
+
+print(dS)
+print(dE)
+print(dr)
