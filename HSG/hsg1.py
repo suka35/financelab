7c63b7fe40d2e3be526a36a5092c5d1ac6fa993a
Sunghan Kim <53588299+sunghan0811@users.noreply.github.com>
2020-05-22 17:18:05 +0900

    Update hsg1.py

--- HSG/hsg1.py
+++ HSG/hsg1.py
@@ -1,20 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 22 15:07:40 2020
-
-@author: sungh
-"""
 import numpy as np
+import matplotlib.pyplot as plt
 
-pai_s = 1   ## volatility of domestic stocks
-lamda_s = 1  ## risk premium of domestic stocks
+sigma_s = 1   ## volatility of domestic stocks
+lamda_s = 1  ## risk premium of domestic stocks (MPR)
 dz_s = 1 ## browninan motion of uncertainty of domestics stocks
+dt = 1 ## 만기/거래일
+r = 0.0286 ## 단기이자율
 
-pai_e = 1  ## volatility of domestic ESG stocks
+sigma_e = 1  ## volatility of domestic ESG stocks
 lamda_e = 1 ## risk premium of domestic ESG stocks
 dz_e = 1 ## browninan motion of uncertainty of domestics ESG stocks
 
-pai_r = 1 ## 국내 명목 이자율에 대한 변동성
+sigma_r = 1 ## 국내 명목 이자율에 대한 변동성
 k = 1 ## 국내 단기이자율의 평균으로의 회귀속도
 r_bar = 1 ## 국내 단기이자율의 장기평균
 dz_r = 1 ## 국내 이자율에대한 불확실성을 나타내는 브라우니안 모션
@@ -30,15 +27,15 @@ x_s = 1 ## 국내주식의 자산배분
 x_e = 1 ## 국내ESG주식의 자산배분
 x_r = 1 ## 국내채권의 자산배분
 
-pai_sf = 1   ## 해외 주가수익률의 변동성
+sigma_sf = 1   ## 해외 주가수익률의 변동성
 lamda_sf = 1  ## 해외 주식에 대한 위험 프리미엄
-dz_sf = 1 ## 해외 주식에 대한 불확실성을 나타내는 브라우니안 모
+dz_sf = 1 ## 해외 주식에 대한 불확실성을 나타내는 브라우니안 모션
 
-pai_ef = 1  ## 해외 ESG주가 수익률의 변동성
+sigma_ef = 1  ## 해외 ESG주가 수익률의 변동성
 lamda_ef = 1 ## 해외 ESG주식에 대한 위험 프리미엄
 dz_ef = 1 ## 해외 ESG 주식에 대한 불확실성을 나타내는 브라우니안 모션
 
-pai_rf = 1 ## 해외 명목 이자율에 대한 변동성
+sigma_rf = 1 ## 해외 명목 이자율에 대한 변동성
 kf = 1 ## 해외 단기이자율의 평균으로의 회귀속도
 rf_bar = 1 ## 해외 단기이자율의 장기평균
 dz_rf = 1 ## 해외 이자율에대한 불확실성을 나타내는 브라우니안 모션
@@ -51,4 +48,10 @@ x_sf = 1 ## 해외주식의 자산배분
 x_ef = 1 ## 해외ESG주식의 자산배분
 x_rf = 1 ## 해외채권의 자산배분
 
-## 날래날래 접속하라우
+A_s = r+(sigma_s*lamda_s)
+B_s = sigma_s
+A_e = r+(sigma_e*lamda_e)
+B_e = sigma_e
+
+dS = (A_s*dt) + (B_s*dz_s)
+dE = (A_e*dt) + (B_e*dz_e)
