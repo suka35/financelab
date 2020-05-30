676a8db8ea1ea97ab9be7d3ddf60644e150c1c90
GabrielRhee11 <j.seok6313@gmailcom>
2020-05-30 18:02:51 +0900

    Update FB.py

--- paper/FB.py
+++ paper/FB.py
@@ -1,3 +1,39 @@
-sigma_s = 1 # 국내 주가지수 수익률의 변동성
-sigma_1 = 1 # 국내 단기이자율(무위험)의 변동성
-sigma_2 = 1 # 해외 단기이자율(무위험)의 변동성
+### Module
+
+import numpy as np
+import math as m
+import matplotlib.pyplot as plt
+
+
+
+### Variable
+
+sigma_s = 1                # 국내 주가지수 수익률의 변동성
+sigma_1 = 1                # 국내 단기이자율(무위험)의 변동성
+sigma_2 = 1                # 해외 단기이자율(무위험)의 변동성
+rho_s1  = 1
+
+
+
+
+
+### Domestic Market
+
+dI = (r + (sigma_s*lam_s)*dt) + (sigma_s*dz_s)         # GBM           eq(1)
+                                                       # Vasicek       eq(2)
+
+### Foreign Market
+
+
+
+
+
+
+### Exchange yudo
+
+
+
+
+
+
+### Dynamics of Bond Price
